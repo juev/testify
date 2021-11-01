7ec5ff8f606d7a4570ff1346fb43ce90825fe398
Evsyukov Denis <denis@evsyukov.org>
2021-11-01 14:57:47 +0300

    [~] use google/gofuzz for converting

--- internal/testify/testify_fuzz.go
+++ internal/testify/testify_fuzz.go
@@ -2,13 +2,11 @@
 
 package testify
 
-import "strconv"
+import fuzz "github.com/google/gofuzz"
 
 func Fuzz(data []byte) int {
-	num, err := strconv.Atoi(string(data))
-	if err != nil {
-		return 0
-	}
+	var num int
+	fuzz.NewFromGoFuzz(data).Fuzz(&num)
 	if _, err := F(num); err != nil {
 		return 0
 	}
